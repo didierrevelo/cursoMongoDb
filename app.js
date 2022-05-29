be555a0f151da7ebcd5e2569b30161a86e7f78d5
didierrevelo <1829@holbertonschool.com>
2022-05-29 18:43:27 -0500

    initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port} http://localhost:${port}`);
+});
